189229ce67b24f9409719469f6803319f26aa642
Andres Pineda <humb.amp123@gmail.com>
2017-05-19 17:53:22 -0700

    change includes to inc

--- 6_birds_ruby_projstart/proj_scipt.rb
+++ 6_birds_ruby_projstart/proj_scipt.rb
@@ -83,7 +83,7 @@ def nextpart(arg, path, makefile, libft)
     if yesorno == 1
       system "mkdir '#{path}/src'"
       system "git clone '#{libft}' '#{path}/src/lib/libft'"
-      system "mkdir '#{path}/includes'"
+      system "mkdir '#{path}/inc'"
       system "touch '#{path}/author' && echo $USER >> #{path}/author"
       system "touch '#{path}/run' && echo 'make -C src/lib/libft/ re\nmake re\nclang -Wall -Wextra -Werror -I includes/ -o main.o -c main.c -g \nclang -o test_#{arg} main.o src/lib/lib#{arg}.a -I includes/ src/lib/libft/ -lft -g \nrm main.o' >> #{path}/run"
       system "rm -rf '#{path}/src/lib/libft/.git'"
